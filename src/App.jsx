0e903fe4f01c51bd39ef03fedf2f11c6bf6e95c0
Pranay Sherkar <116248469+Pranaysherkar@users.noreply.github.com>
2024-02-21 13:36:14 +0530

    Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const name = useRef(null);
-  const age = useRef(null);
-  const handleEvent=(event)=>{
-    event.preventDefault();
-    console.log(name.current.value ,age.current.value)
-  }
+ const [val,setVal]= useState({name:""});
+ const handel=(prev)=>{
+    prev.preventDefault();
+    console.log(prev.target.value);
+
+
+ }
   return (
     <>
       <div className="w-full h-screen flex items-center justify-center">
-        <form action="" className="border-black border-2 p-5 flex flex-col items-center " onSubmit={handleEvent}>
+        <form action="" className="border-black border-2 p-5 flex flex-col items-center ">
           <h1 className="text-2xl pb-2">FormHandling</h1>
-          <input ref={name} className="text-xl border-solid border-2 border-black p-2 m-2" placeholder="Name"  type="text" />
-          <input ref={age}  className="text-xl border-solid border-2 border-black p-2 m-2" placeholder="Age" type="text" />
-          <button className="rounded-md border-2 border-black p-2  m-2"  type="submit">submit</button>
+          <input  onChange={handel}className="text-xl border-solid border-2 border-black p-2 m-2" placeholder="Name"  type="text" />
+          <input className="text-xl border-solid border-2 border-black p-2 m-2" placeholder="Age" type="text" />
+          <button  className="rounded-md border-2 border-black p-2  m-2"  type="submit">submit</button>
         </form>
       </div>
     </>
